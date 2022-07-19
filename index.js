a5ff5847cf8f7f4f5e088c8a656e520543f35f84
rasvindra <rasvindra@hotmail.com>
2022-07-19 07:57:50 -0700

    Finished questions and creating README file

--- index.js
+++ index.js
@@ -1,46 +1,56 @@
-const inquirer = require("inquirer");
+const fs = require('fs')
+const inquirer = require('inquirer');
 
-inquirer.prompt([ {
-    message: "What is Your First and Last Name?",
-    type: "utf8",
-    name: userName,
+inquirer.prompt ([  {
+    message: "What is your project title?",
+    type: "input",
+    name: "Project Name",
 },
 {
-    message: "What is Your First and Last Name?",
-    type: "utf8",
-    name: userName,
+    message: "Please enter a brief description of your project.",
+    type: "input",
+    name: "Project Description",
 },
 {
-    message: "What is Your First and Last Name?",
-    type: "utf8",
-    name: userName,
+    message: "What are the steps needed to install your apllication?",
+    type: "input",
+    name: "Project Install Intructions",
 },
 {
-    message: "What is Your First and Last Name?",
-    type: "utf8",
-    name: userName,
+    message: "What is your GitHub Username?",
+    type: "input",
+    name: "GitHub Link",
 },
 {
-    message: "What is Your First and Last Name?",
-    type: "utf8",
-    name: userName,
+    message: "What is your e-mail address?",
+    type: "input",
+    name: "User Email",
 },
 {
-    message: "What is Your First and Last Name?",
-    type: "utf8",
-    name: userName,
+    message: "Please include contribution notes for your application.",
+    type: "input",
+    name: "Project Contribution",
 },
+{
+    message: "Please include any usage information for your aplication.",
+    type: "input",
+    name: "Project Usage",
+},
+{
+    message: "Please include any test instructions for your application.",
+    type: "input",
+    name: "Project Test",
+},
+{
+    message: "Please choose your license from the options provided.",
+    type: "list",
+    name: "Project License",
+    choices: ['IBM','MIT','MOZILLA','APACHE']
+},
+]).then ((ans) => {
+    console.log(ans);
+    fs.writeFile("./README.MD",JSON.stringify(ans, null,1),function(err){
+        if (err) console.log ("error",err)
 
-
-
-
-
-
-
-
-
-
-
-
-
-])
+    })
+})
