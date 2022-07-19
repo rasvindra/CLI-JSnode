00f51bebf23d4a028eab64a36296b6f230e540fc
rasvindra <rasvindra@hotmail.com>
2022-07-18 21:17:05 -0700

    created repo and index.js.

---
+++ index.js
@@ -0,0 +1,46 @@
+const inquirer = require("inquirer");
+
+inquirer.prompt([ {
+    message: "What is Your First and Last Name?",
+    type: "utf8",
+    name: userName,
+},
+{
+    message: "What is Your First and Last Name?",
+    type: "utf8",
+    name: userName,
+},
+{
+    message: "What is Your First and Last Name?",
+    type: "utf8",
+    name: userName,
+},
+{
+    message: "What is Your First and Last Name?",
+    type: "utf8",
+    name: userName,
+},
+{
+    message: "What is Your First and Last Name?",
+    type: "utf8",
+    name: userName,
+},
+{
+    message: "What is Your First and Last Name?",
+    type: "utf8",
+    name: userName,
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+])
